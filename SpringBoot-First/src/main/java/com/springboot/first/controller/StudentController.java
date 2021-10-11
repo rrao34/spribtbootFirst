d2fd37c8352565822d690ca5612a190cf18d1184
e3rrao <e3rrao@xenw764rsa016.corp.generali.net>
2021-10-11 18:54:44 +0530

    HATEOAS Configurations and Test

--- SpringBoot-First/src/main/java/com/springboot/first/controller/StudentController.java
+++ SpringBoot-First/src/main/java/com/springboot/first/controller/StudentController.java
@@ -1,8 +1,16 @@
 package com.springboot.first.controller;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Links;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot.first.bean.StudentBean;
 import com.springboot.first.service.StudentService;
@@ -36,14 +45,37 @@ public class StudentController {
 		return studentService.getStudentById(id);
 	}
 
-	@PostMapping(value="/newstudent")
-	public void addNewStudent(@Validated @RequestBody StudentBean newStudent) {
+	@PostMapping(value = "/newstudent")
+	public ResponseEntity<Object> addNewStudent(@Validated @RequestBody StudentBean newStudent) {
 		studentService.addNewStudent(newStudent);
+
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(newStudent.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).build();
 	}
 	
 	@DeleteMapping(path = "/students/{id}")
 	public  void deleteStudentById(@PathVariable int id) {
 		studentService.deleteStudentById(id);
+	}
+	
+	//HATEOAS
+	@GetMapping(path = "/studentsHATEOAS/{id}")
+	public  RepresentationModel getstudentsHATEOASById(@PathVariable int id) {
+		StudentBean student = studentService.getStudentById(id);
+		
+		Link link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getStudents()).withRel("All-Students");
+		
+		List<Link> links = new ArrayList<>();
+		links.add(link);
+		
+		RepresentationModel<?> collection = CollectionModel.of(student, links);
+		
+		return collection;
 	}
 
 }
